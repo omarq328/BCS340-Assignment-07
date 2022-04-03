3d3f35e9b8ecb0ee9e389835ec4ad5b9f9f20158
loboka1 <102673619+loboka1@users.noreply.github.com>
2022-04-02 23:25:59 -0400

    Added selection + bubble sort, + printArray method

--- Assignment7Functions.cpp
+++ Assignment7Functions.cpp
@@ -13,6 +13,13 @@ void randomArraySet(int arr[], int num)
     }
 }
 
+//Swap function
+void swap(int arr[], int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
 //Selection Sort
 void selectionSort(int arr[], int n) {
   int outer, inner, min;
@@ -32,8 +39,36 @@ void selectionSort(int arr[], int n) {
 }
 
 //Insertion Sort
+void insertionSort(int arr[], int n) {
+  int i, key, j;
+  for (i = 1; i < n; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  for (int i = 0; i < 50; i++) {
+    cout << arr[i] << " ";
+  }
+}
 
 //Bubble Sort
+void bubbleSort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  for (int i = 0; i < 50; i++) {
+    cout << arr[i] << " ";
+  }
+}
 
 //Merge Sort
 void merge(int arr[], int l, int m, int r) {
@@ -160,6 +195,13 @@ void heapSort(int arr[], int n)
 }
 
 //Print Array Function(Prints first 50 of the array)
+void printArray(int A[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << A[i] << ",";
+  cout << endl;
+}
+
 
 // Function to reset the array
 void resetArray(int arrOne[], int arrTwo[], int num)
