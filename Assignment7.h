a2e0e52a900924a2c57322e1b3db9b7e36139f2b
loboka1 <102673619+loboka1@users.noreply.github.com>
2022-04-02 23:29:36 -0400

    Added Insertion + bubble Sort, + printArray

--- Assignment7.h
+++ Assignment7.h
@@ -5,13 +5,17 @@
 void printMenu();
 int menu(int answer, int arr[], int arrTwo[], int num);
 void copyArray(int arrOne[], int arrTwo[], int num);
+void printArray(int A[], int size);
 void randomArraySet(int arr[], int num);
+void swap(int arr[], int i, int j);
 void resetArray(int arrOne[], int arrTwo[], int num);
 void heapSort(int arr[], int n);
 void heapify(int arr[], int n, int i);
 void merge(int arr[], int l, int m, int r);
 void mergeSort(int arr[], int l, int r);
 void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n);
+void bubbleSort(int arr[], int n);
 void quickSort(int arr[], int left, int right);
 
 
