fdc617dd4fbf5082fcf610d50eb02af36c360595
omarq328 <76020064+omarq328@users.noreply.github.com>
2022-04-02 21:11:02 -0400

    Update Assignment7.h

--- Assignment7.h
+++ Assignment7.h
@@ -1,1 +1,15 @@
+#ifndef Assignment7_H
+#define Assignment7_H
 
+
+void printMenu();
+int menu(int answer, int arr[], int arrTwo[], int num);
+void bubbleSort(int arr[], int n);
+void copyArray(int arrOne[], int arrTwo[], int num);
+void randomArraySet(int arr[], int num);
+void resetArray(int arrOne[], int arrTwo[], int num);
+void heapSort(int arr[], int n);
+void heapify(int arr[], int n, int i);
+
+
+#endif 
