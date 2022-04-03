bd3becffd6f7e78d730c57774512507ab5ae621a
onwae4 <50401639+onwae4@users.noreply.github.com>
2022-04-02 21:24:29 -0400

    Update Assignment7.h

--- Assignment7.h
+++ Assignment7.h
@@ -11,6 +11,8 @@ void heapSort(int arr[], int n);
 void heapify(int arr[], int n, int i);
 void merge(int arr[], int l, int m, int r);
 void mergeSort(int arr[], int l, int r);
+void selectionSort(int arr[], int n);
+void quickSort(int arr[], int left, int right);
 
 
 #endif 
