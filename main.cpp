37a1859331e3428da3a5d8acb44d766cc266b289
omarq328 <76020064+omarq328@users.noreply.github.com>
2022-04-02 21:11:59 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,46 @@
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include "Assignment7.h"
+using namespace std;
 
+
+
+int main() {
+int choice, choiceTwo;
+    clock_t start, end;
+    
+    int num = 10000;
+    int low = 0;
+    int high = num - 1;
+
+    int arr[num];
+    randomArraySet(arr, num);
+
+    int copyArr[num];
+    copyArray(arr, copyArr, num);
+do {
+  start = clock();
+  system("clear");
+    cout << "============================" << endl;
+    printMenu();
+    cout << "============================" << endl;
+    cout << "Please select an option: ";
+    cin >> choice;
+system("clear");
+    menu(choice, arr, copyArr, num);
+      end = clock();
+    double time_taken = 1000 * double(end - start) / double(CLOCKS_PER_SEC);
+    cout << endl
+         << "Time taken by program is : " << fixed << time_taken
+         << setprecision(5);
+    cout << " ms " << endl;
+  cout << "Enter 0 to exit or any number to go back to menu! " << endl;
+  cin >> choiceTwo;
+  if (choiceTwo == 0)
+    return 0;
+
+  }while (choice != 0);
+
+  
+}
